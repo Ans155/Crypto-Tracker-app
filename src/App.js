628476cd02c06795eaa1d308eba63fc5e2fe01e2
Ansh <anshchoudhary155@gmail.com>
2022-09-27 01:57:09 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import {BrowserRouter, Route} from "react-router-dom";
+import Header from "./components/Header";
+import Homepage from "./Pages/Homepage";
+import coinPage from "./Pages/coinPage";
+import { makeStyles } from '@material-ui/core';
+
+function App() {
+
+  const useStyles=makeStyles(() => ({
+    App :{
+      backgroundColor:"#14161a",
+      color:"white",
+      minHeight: "100vh",
+    },
+
+  }));
+
+  const classes=useStyles();
+  return (
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        <Route path='/' component={Homepage} exact />
+        <Route path="/coins/:id" component={coinPage} />
+
+      </div>
+    </BrowserRouter>
+ 
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import { AppBar,Container,Toolbar,Typography,Select,MenuItem } from "@material-ui/core";
+import { makeStyles } from '@material-ui/styles';
+import Avatar from '@material-ui/core/Avatar';
+import { useHistory } from 'react-router-dom';
+import icon from "../images/3888px-Cryptocurrency_Logo.svg.png";
+import { CryptoState } from '../CryptoContext';
+
+const useStyles=makeStyles(() => ({
+  title:
+  {
+    flex:1,
+    color: "purple",
+    fontFamily: "Montserrat",
+    fontWeight:"bold",
+    cursor:"pointer",
+
+  }
+ 
+
+}));
+
+const Header = () => {
+
+  const classes= useStyles();
+  const history=useHistory();
+  const { currency, setCurrency } = CryptoState();
+  return (
+    <AppBar color="white" position="static">
+      <Container>
+        <Toolbar>
+        <Avatar 
+        onClick={() => history.push("/")}
+        style={{
+          marginRight:10,
+        }}
+        src={icon}/>
+          <Typography 
+          onClick={() => history.push("/")} 
+          className={classes.title}>
+            Crypto Tracker
+          </Typography>
+          <Select
+            value={currency}
+            onChange={(e) => setCurrency(e.target.value)}
+            variant="outlined"
+            style={{
+              border: '1px solid rgba(0, 0, 0, 2)',
+              
+              borderBlockColor:"black",
+              color:"black",
+              width:100,
+              height:40,
+              marginRight:15,
+            }}>
+            <MenuItem value={"USD"}>
+              USD
+            </MenuItem>
+            <MenuItem value={"INR"}>
+              INR
+            </MenuItem>
+          </Select>
+
+        </Toolbar>
+      </Container>
+
+    </AppBar>
+   
+  )
+}
+
+export default Header
