d2bfb5ea7c8031579fb86a82aa474cc3b150812e
Ans155 <110165397+Ans155@users.noreply.github.com>
2023-07-23 02:17:05 +0530

    final login and signup component

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -1,34 +1,65 @@
-import { Box, Button, TextField } from "@material-ui/core";
+import { Box, Button, TextField, Typography } from "@material-ui/core";
+import { Lock } from "@material-ui/icons";
 import { useState } from "react";
 import { CryptoState } from "../../CryptoContext";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase";
 
 const Login = ({ handleClose }) => {
-  const customStyles = {
-    color: 'red',
-  };
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const handleSubmit = async () => {};
-  
+  const [error, setError] = useState("");
+
+  const customStyles = {
+    color: "red",
+  };
+
+  const { setAlert } = CryptoState();
+
+  const handleSubmit = async () => {
+    if (!email || !password) {
+      setError("Please fill all the fields");
+      return;
+    }
+
+    try {
+      const result = await signInWithEmailAndPassword(auth, email, password);
+      setAlert({
+        open: true,
+        message: `Login Successful. Welcome ${result.user.email}`,
+        type: "success",
+      });
+
+      handleClose();
+    } catch (error) {
+      setError(error.message);
+    }
+  };
 
   return (
     <Box
       p={3}
       style={{
+        backgroundColor: "#F5F5F5",
+        borderRadius: "8px",
+        boxShadow: "0px 2px 10px rgba(0, 0, 0, 0.1)",
         display: "flex",
         flexDirection: "column",
         gap: "20px",
-        color:"black",
       }}
     >
+      <Typography variant="h4" style={{ color: "#333333" }}>
+        Login
+      </Typography>
       <TextField
         variant="outlined"
         type="email"
         label="Enter Email"
         InputProps={{
-          style: customStyles,
+          style: {
+            color: "black", // Change text color to black
+            borderBottomColor: "#EEBC1D", // Change bottom border color to yellow
+          },
         }}
         value={email}
         onChange={(e) => setEmail(e.target.value)}
@@ -40,22 +71,43 @@ const Login = ({ handleClose }) => {
         type="password"
         value={password}
         InputProps={{
-          style: customStyles,
+          style: {
+            color: "black", // Change text color to black
+            borderBottomColor: "#EEBC1D", // Change bottom border color to yellow
+          },
+          endAdornment: (
+            <Lock
+              style={{
+                color: customStyles.color,
+                fontSize: "1.5rem",
+              }}
+            />
+          ),
         }}
         onChange={(e) => setPassword(e.target.value)}
         fullWidth
       />
+      {error && (
+        <Typography variant="body1" style={{ color: "red" }}>
+          {error}
+        </Typography>
+      )}
       <Button
         variant="contained"
         size="large"
-        
-        style={{ backgroundColor: "#EEBC1D" }}
+        style={{ backgroundColor: "#EEBC1D", color: "black", transition: "0.3s" }} // Change text color to black
         onClick={handleSubmit}
+        onMouseEnter={(e) => {
+          e.target.style.backgroundColor = "#FFD700";
+        }}
+        onMouseLeave={(e) => {
+          e.target.style.backgroundColor = "#EEBC1D";
+        }}
       >
-        Sign Up
+        Login
       </Button>
     </Box>
   );
 };
 
-export default Login
+export default Login;
--- src/components/Authentication/Signup.js
+++ src/components/Authentication/Signup.js
@@ -1,35 +1,31 @@
-import { Box, Button, TextField } from "@material-ui/core";
+import { Box, Button, TextField, Typography } from "@material-ui/core";
 import { useState } from "react";
 import { CryptoState } from "../../CryptoContext";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase";
 
 const Signup = ({ handleClose }) => {
-  const customStyles = {
-    color: 'red',
-  };
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const customStyles = {
+    color: "red",
+  };
 
   const { setAlert } = CryptoState();
 
   const handleSubmit = async () => {
+    setError(""); // Reset error message before validating
+
     if (password !== confirmPassword) {
-      setAlert({
-        open: true,
-        message: "Passwords do not match",
-        type: "error",
-      });
+      setError("Passwords do not match");
       return;
     }
 
     try {
-      const result = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
+      const result = await createUserWithEmailAndPassword(auth, email, password);
       setAlert({
         open: true,
         message: `Sign Up Successful. Welcome ${result.user.email}`,
@@ -43,27 +39,33 @@ const Signup = ({ handleClose }) => {
         message: error.message,
         type: "error",
       });
-      return;
     }
-
   };
-  
 
   return (
     <Box
       p={3}
       style={{
+        backgroundColor: "#F5F5F5",
+        borderRadius: "8px",
+        boxShadow: "0px 2px 10px rgba(0, 0, 0, 0.1)",
         display: "flex",
         flexDirection: "column",
         gap: "20px",
       }}
     >
+      <Typography variant="h4" style={{ color: "#333333" }}>
+        Sign Up
+      </Typography>
       <TextField
         variant="outlined"
         type="email"
         label="Enter Email"
         InputProps={{
-          style: customStyles,
+          style: {
+            color: "black", // Change text color to black
+            borderBottomColor: "#EEBC1D", // Change bottom border color to yellow
+          },
         }}
         value={email}
         onChange={(e) => setEmail(e.target.value)}
@@ -74,7 +76,10 @@ const Signup = ({ handleClose }) => {
         label="Enter Password"
         type="password"
         InputProps={{
-          style: customStyles,
+          style: {
+            color: "black", // Change text color to black
+            borderBottomColor: "#EEBC1D", // Change bottom border color to yellow
+          },
         }}
         value={password}
         onChange={(e) => setPassword(e.target.value)}
@@ -85,17 +90,31 @@ const Signup = ({ handleClose }) => {
         label="Confirm Password"
         type="password"
         InputProps={{
-          style: customStyles,
+          style: {
+            color: "black", // Change text color to black
+            borderBottomColor: "#EEBC1D", // Change bottom border color to yellow
+          },
         }}
         value={confirmPassword}
         onChange={(e) => setConfirmPassword(e.target.value)}
         fullWidth
       />
+      {error && (
+        <Typography variant="body1" style={{ color: "red" }}>
+          {error}
+        </Typography>
+      )}
       <Button
         variant="contained"
         size="large"
-        style={{ backgroundColor: "#EEBC1D" }}
+        style={{ backgroundColor: "#EEBC1D", color: "black", transition: "0.3s" }} // Change text color to black
         onClick={handleSubmit}
+        onMouseEnter={(e) => {
+          e.target.style.backgroundColor = "#FFD700";
+        }}
+        onMouseLeave={(e) => {
+          e.target.style.backgroundColor = "#EEBC1D";
+        }}
       >
         Sign Up
       </Button>
